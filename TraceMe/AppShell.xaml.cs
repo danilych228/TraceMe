b195f662a8cc29433d0328c69a86122d2c510fdc
hp <hp@DESKTOP-DVCVLAF>
2025-09-20 18:19:47 +0300

    Добавьте файлы проекта.

---
+++ TraceMe/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TraceMe
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TraceMe/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TraceMe
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
